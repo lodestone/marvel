21d57be36cc3abc4b82b6630879c6d51bf242bfa
Matt Petty <matt@kizmeta.com>
2014-02-03 01:26:57 -0600

    Handle parsing of nested resources

--- lib/marvel.rb
+++ lib/marvel.rb
@@ -35,15 +35,31 @@ end
 
 class MarvelParser < Faraday::Response::Middleware
   def on_complete(env)
+    puts env
     json = JSON.parse(env[:body], :symbolize_names => true)
-    json = json[:data][:results] || json[:data][:result]
-    errors = json.delete(:errors) || {}
-    metadata = json.delete(:metadata) || []
-    env[:body] = { :data => json, :errors => errors, :metadata => metadata }
+    if json[:code] == 200
+      json = json[:data][:results] || json[:data][:result]
+      json.each do |j|
+        j[:characters] = j[:characters][:items] if j[:characters].present?
+        j[:comics]  = j[:comics][:items] if j[:comics].present?
+        j[:creators] = j[:creators][:items] if j[:creators].present?
+        j[:events]  = j[:events][:items] if j[:events].present?
+        j[:series] = j[:series][:items] if j[:series].present?
+        j[:stories] = j[:stories][:items] if j[:stories].present?
+      end
+      json = json.first if json.length == 1
+      errors = json.delete(:errors) || {}
+      metadata = json.delete(:metadata) || []
+      env[:body] = { :data => json, :errors => errors, :metadata => metadata }
+    else
+      # TODO: Raise an error
+      env[:body] = { :data => json, :errors => json[:status], :metadata => metadata }
+    end
   end
 end
 
 Her::API.setup url: "https://gateway.marvel.com/v1/public" do |c|
+  # Authentication
   c.use SetAuthParams
   # Request
   c.use Faraday::Request::UrlEncoded
@@ -59,26 +75,63 @@ module Marvel;end
 # TODO: Watch these and see if they make sense breaking out into separate files
 class Marvel::Base
   include Her::Model
+
+  def id
+    resourceURI.split("/").last
+  end
+
 end
 
 class Marvel::Character < Marvel::Base
+  # Attributes => ["id", "name", "description", "modified", "thumbnail", "resourceURI", "comics", "series", "stories", "events", "urls"]
+  has_many :comics
+  has_many :events
+  has_many :stories
 end
 
 class Marvel::Comic < Marvel::Base
+  # Attributes => ["id", "digitalId", "title", "issueNumber", "variantDescription", "description", "modified", "isbn", "upc", "diamondCode", "ean", "issn", "format", "pageCount", "textObjects", "resourceURI", "urls", "series", "variants", "collections", "collectedIssues", "dates", "prices", "thumbnail", "images", "creators", "characters", "stories", "events"]
+  has_many :characters
+  has_many :creators
+  has_many :events
+  has_many :stories
 end
 
 class Marvel::Creator < Marvel::Base
+  # Attributes => ["resourceURI", "name", "role", "comic"]
+  has_many :comics
+  has_many :events
+  has_many :stories
 end
 
 class Marvel::Event < Marvel::Base
+  # Attributes => ["id", "title", "description", "resourceURI", "urls", "modified", "start", "end", "thumbnail", "creators", "characters", "stories", "comics", "series", "next", "previous"]
+  has_many :characters
+  has_many :comics
+  has_many :creators
+  has_many :stories
 end
 
 class Marvel::Series < Marvel::Base
+  # Attributes => ["available", "collectionURI", "items", "returned"]
+  has_many :characters
+  has_many :comics
+  has_many :creators
+  has_many :events
+  has_many :stories
 end
 
 class Marvel::Story < Marvel::Base
+  # Attributes => ["resourceURI", "name", "type", "character"]
+  has_many :characters
+  has_many :comics
+  has_many :creators
+  has_many :events
 end
 
+# TODO: Thumbnail class to instantiate thumbnail data in a useful way
+# class Marvel::Thumbnail
+# end
 
 
 
